e26d9e0bef9155dcc6bb9562506abd4660cc97bb
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2019-01-14 10:49:16 +0300

    - Migrating Carthage to separate project

---
+++ CarthageSupport/StretchScrollView/StretchScrollView.h
@@ -0,0 +1,19 @@
+//
+//  StretchScrollView.h
+//  StretchScrollView
+//
+//  Created by mac-246 on 1/11/19.
+//  Copyright © 2019 Anton Plebanovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StretchScrollView.
+FOUNDATION_EXPORT double StretchScrollViewVersionNumber;
+
+//! Project version string for StretchScrollView.
+FOUNDATION_EXPORT const unsigned char StretchScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StretchScrollView/PublicHeader.h>
+
+
